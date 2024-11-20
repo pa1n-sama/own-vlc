c9d7785488a33f0d413fbb3b8b3566cb0f70ae5a
Pain-sama <145512477+pa1n-sama@users.noreply.github.com>
2024-11-20 03:14:14 +0100

    Update mainwindow.h

--- main/mainwindow.h
+++ main/mainwindow.h
@@ -16,9 +16,18 @@
 #include <QKeyEvent>
 #include <QUrl>
 
+
+
+
 class MainWindow: public QMainWindow{
     Q_OBJECT
 public:
+    enum RepeatMode{
+        PlaylistRepeat=0,
+        VideoRepeat=1,
+        Shuffle=2,
+    };
+
     MainWindow(QWidget *parent=nullptr);
     void firstlayoutclick(int buttonindex);
     void fourthlayoutclick(int buttonindex);
@@ -26,17 +35,18 @@ public:
     void setsliderposition(qint64 position);
     void mediaposition(int position);
     void keyPressEvent(QKeyEvent *event)override;
-    void mediaplayer(QString url="image.png");
+    void mediaplayer(QString url="blackscreen");
     void onMediaStatusChanged(QMediaPlayer::MediaStatus status);
-    ~MainWindow();
+
 private:
-    QString playertype;
+    bool paused = false;
+    bool fullscreened = false;
     bool finished = false;
-    QLabel* image;
-    int  rep = 0 ;
+    QString playertype;
     size_t videoindex=0;
-    QList<QString> firstlayoutbuttons = {"Media","Playback","Audio","Video","Subtitle","Tools","View","Help"};
-    QList<QString> actionslist = {"open file","open folder"};
+    RepeatMode  rep = PlaylistRepeat ;
+    
+    QLabel* image;
     QWidget *mainwidget;
     QVBoxLayout *mainlayout;
     QHBoxLayout *firstlayout;
@@ -47,12 +57,13 @@ private:
     QAudioOutput *audio;
     QVideoWidget *video;
     QSlider *videoslider;
-    QList<QString> mcbuttons = {"BPause","BBack","BStop","BNext","BFullscreen","BPlaylist","BRepeating"};
     QLabel *currenttimer;
     QLabel *totaltimer;
-    bool paused = false;
-    bool fullscreened = false;
+
     std::vector<QUrl> playlist;
+    QList<QString> mcbuttons = {"BPause","BBack","BStop","BNext","BFullscreen","BPlaylist","BRepeating"};
+    QList<QString> firstlayoutbuttons = {"Media","Playback","Audio","Video","Subtitle","Tools","View","Help"};
+    QList<QString> actionslist = {"open file","open folder"};
 
 };
 

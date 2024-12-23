4326d91bdbcc08b3bd47e1f54c99bf322ec92e50
Pain-sama <145512477+pa1n-sama@users.noreply.github.com>
2024-12-23 23:46:22 +0100

    Update mediaurl.h
    moving the constructor to the header

--- main/mediaurl.h
+++ main/mediaurl.h
@@ -8,12 +8,47 @@
 #include <QTextEdit>
 #include <QPushButton>
 #include <QString>
+#include <QFile>
 
 class UrlWindow : public QDialog {
     Q_OBJECT
 
 public:
-    explicit UrlWindow(QWidget *parent = nullptr);
+
+    //constructor for the url window input
+    UrlWindow(QWidget *parent = nullptr):QDialog(parent){
+        this->setFocus();
+        QFile file("/home/pain/.config/VFW/cache/styles/secondwindow.css");
+        if (file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+            QTextStream in(&file);
+            QString styleSheet = in.readAll();
+            this->setStyleSheet(styleSheet);
+        }
+        this->setFixedSize(800,500);
+        mainlayout = new QVBoxLayout;
+        firstlayout = new QHBoxLayout;
+        secondlayout = new QHBoxLayout;
+        urllabel = new QLabel("URL: ");
+        urlentry = new QTextEdit;
+        donebutton = new QPushButton("OK");
+        donebutton->setObjectName("okbutton");
+        cancelbutton=new QPushButton("Cancel");
+        connect(donebutton,&QPushButton::clicked,[this](){
+            url = urlentry->toPlainText();
+            QDialog::accept();
+        });
+        connect(cancelbutton,&QPushButton::clicked,[this](){
+            QDialog::accept();
+        });
+        firstlayout->addWidget(urllabel);
+        firstlayout->addWidget(urlentry);
+        secondlayout->addWidget(donebutton);
+        secondlayout->addWidget(cancelbutton);
+        mainlayout->addLayout(firstlayout);
+        mainlayout->addLayout(secondlayout);
+        setLayout(mainlayout);
+        setWindowTitle("URL Window");
+    }
 
 public:
     QString url="";
